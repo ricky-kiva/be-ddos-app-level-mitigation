5749766bcac49a8bafbdce771d3864cc6eaba8e1
ricky-kiva <ricky.kiva@gmail.com>
2024-12-16 11:18:30 +0700

    feat(index): impl. manual IP block on all routes

--- index.js
+++ index.js
@@ -8,13 +8,27 @@ const init = async () => {
     port: 3000
   });
 
+  server.ext('onRequest', (req, h) => {
+    if (req.info.remoteAddress === '127.0.0.1'
+      || req.info.remoteAddress === '::1') {
+      return h
+        .response('You cannot make request')
+        .code(403)
+        .takeover();
+    }
+
+    return h.continue;
+  });
+
   server.route({
     method: 'GET',
     path: '/home',
     handler: (req, h) => {
       if (req.info.remoteAddress === '127.0.0.1' // blocks IPv4
         || req.info.remoteAddress === '::1') { // blocks IPv6
-        return h.response('You cannot make request').code(403);
+        return h
+          .response('You cannot make request')
+          .code(403);
       }
 
       return h
